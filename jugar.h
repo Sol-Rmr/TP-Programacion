df2f3014dd154089eeb03939bc7726eaf8c5ec28
Sol-Rmr <solabigailromero500@gmail.com>
2025-06-27 00:58:48 -0300

    jugar.h

--- jugar.h
+++ jugar.h
@@ -1,6 +1,15 @@
 #ifndef JUGAR_H_INCLUDED
 #define JUGAR_H_INCLUDED
 
+
+void ModoDeJuegoMenu();
+void ModoDeJuegoOpciones();
+
+void gameplayUnJugador();
+void encabezadoUnJugador();
+
+
+/// FUNCIONES DE LA CLASE
 void gameplay();
 int obtenerRandom(int tamanio);
 void generarTirada(int v[], int tamanio);
