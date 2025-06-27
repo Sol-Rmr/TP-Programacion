55e0081551c3c4b3b724dd058ee5d2a3ecb52e9c
Sol-Rmr <solabigailromero500@gmail.com>
2025-06-27 01:13:01 -0300

    Add files via upload

---
+++ creditos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "creditos.h"
+using namespace std;
+
+void MostrarCreditos() {
+    system("cls");
+    cout << "------------ CREDITOS ------------" << endl;
+    cout << "\n";
+    cout << "             GRUPO 17" << endl;
+    cout << "\nNOMBRE                 | LEGAJO" << endl;
+    cout << "\nSol Romero             | 30576" << endl;
+    cout << "Maximo De La Rosa      | 33473" << endl;
+    cout << "\n----------------------------------" << endl;
+}
---
+++ creditos.h
@@ -0,0 +1,6 @@
+#ifndef CREDITOS_H_INCLUDED
+#define CREDITOS_H_INCLUDED
+
+void MostrarCreditos();
+
+#endif // CREDITOS_H_INCLUDED
