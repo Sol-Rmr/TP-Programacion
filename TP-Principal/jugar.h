d01d1a582a983dfb34fd5eae53b97df288832c5d
Sol-Rmr <solabigailromero500@gmail.com>
2025-06-28 21:13:10 -0300

    Update jugar.h

--- TP-Principal/jugar.h
+++ TP-Principal/jugar.h
@@ -22,10 +22,8 @@ void gameplayUnJugador();
 
 
 /// FUNCIONES DE LA CLASE
-void gameplay();
 int obtenerRandom(int tamanio);
 void generarTirada(int v[], int tamanio);
 void mostrarTirada(int v[], int tamanio);
-int obtenerPuntos(int v[], int tamanio);
 
 #endif // JUGAR_H_INCLUDED
