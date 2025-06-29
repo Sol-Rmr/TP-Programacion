0ce69b86225d494dd083c9a9971ae2c44136f534
Sol-Rmr <solabigailromero500@gmail.com>
2025-06-28 21:02:24 -0300

    Update jugar.h

--- TP-Principal/jugar.h
+++ TP-Principal/jugar.h
@@ -1,12 +1,24 @@
 #ifndef JUGAR_H_INCLUDED
 #define JUGAR_H_INCLUDED
 
+using namespace std;
 
 void ModoDeJuegoMenu();
 void ModoDeJuegoOpciones();
 
-void gameplayUnJugador();
+string ingresarNombre();
 void encabezadoUnJugador();
+void mostrarTurno(int &turno, string &nombre, int &puntaje, int &tirada);
+int preguntarFinalizarTurno();
+void mostrarResultado(int puntaje, string combinacion);
+void mostrarFinal (string nombre, int puntaje);
+
+int contar(int dados[], int valor, int tamanio);
+string detectarCombinacion(int dados[], int &puntos, int tamanio);
+
+
+///MODO UN JUGADOR
+void gameplayUnJugador();
 
 
 /// FUNCIONES DE LA CLASE
