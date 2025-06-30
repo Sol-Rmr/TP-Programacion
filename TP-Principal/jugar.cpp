8bdd0ecdc5b19ab3c17901d1d76e36f7937f23da
Sol-Rmr <solabigailromero500@gmail.com>
2025-06-30 20:37:17 -0300

    Update jugar.cpp

--- TP-Principal/jugar.cpp
+++ TP-Principal/jugar.cpp
@@ -18,7 +18,7 @@ void ModoDeJuegoMenu(string nombres[], int puntajes[], int t) {
                 system("pause");
                 break;
             case 2:
-                cout << "MODO DOS JUGADORES GAMEPLAY" << endl;
+                gameplayDosJugadores(nombres, puntajes, t);
                 system("pause");
                 break;
             case 0:
@@ -52,11 +52,15 @@ string ingresarNombre() {
 }
 
 void encabezadoUnJugador() {
-    cout << "---------- MODO UN JUGADOR ----------" << endl;
+    cout << "---------- MODO UN JUGADOR ----------\n" << endl;
+}
+
+void encabezadoDosJugadores() {
+    cout << " ---------- MODO DOS JUGADORES ----------\n" << endl;
 }
 
 void mostrarTurno(int &turno, string &nombre, int &puntaje, int &tirada) {
-    cout << "\nTurno " << turno << " de "  << nombre << endl;
+    cout << "Turno " << turno << " de "  << nombre << endl;
     cout << "Puntaje Total: " << puntaje << endl;
     cout << "\nTirada #" << tirada << endl;
 
@@ -91,6 +95,16 @@ void mostrarFinal (string nombre, int puntaje) {
     cout << "\n\n\nVolviendo al menu de juego..." << endl;
 }
 
+void mostrarFinal2Jugadores (string nombres[], int puntajes[], int tamanio) {
+    system("cls");
+    cout << "---------- Fin del Juego ----------" << endl;
+    for(int i = 0; i < tamanio; i++) {
+        cout << "\nNombre del Jugador "<< i+1<<":  " << nombres[i] << endl;
+        cout << "Puntaje Total:         " << puntajes[i] << endl;
+    }
+        cout << "\n\n\nVolviendo al menu de juego..." << endl;
+}
+
 int obtenerRandom(int tamanio){
     return rand()%tamanio + 1;
 }
@@ -109,6 +123,23 @@ void mostrarTirada(int v[], int tamanio){
     cout << endl;
 }
 
+void generarTiradaTurno(int v[], int tamanio) {
+    for(int i = 0; i < tamanio; i++) {
+        v[i] = obtenerRandom(6);
+    }
+}
+
+void mostrarTiradaTurno(string nombres[], int v[], int tamanio) {
+    for (int i = 0; i < tamanio; i++) {
+        cout << "\nTirada de " << nombres[i] << ": " << v[i] << endl;
+    }
+}
+
+void mostrarPrimerJugador(string nombre) {
+    cout << "\n--------------------------------------------" << endl;
+    cout << nombre << " comienza!" << endl;
+    cout << "--------------------------------------------" << endl;
+}
 
 ///COMBINACIONES
 // Contar cuántas veces aparece un número
@@ -217,6 +248,41 @@ string detectarCombinacion(int dados[], int &puntos, int tamanio) {
 }
 
 
+void definirTurno(string nombres[], int v[], int tamanio) {
+    encabezadoDosJugadores();
+    cout << "|          Definicion de Turnos          |" << endl;
+    generarTiradaTurno(v, tamanio);
+    mostrarTiradaTurno(nombres, v, tamanio);
+    cout << "\n\n";
+
+    if(v[0] == v[1]) {
+        bool empate = true;
+        while (empate) {
+            cout << "\n\n--------------------------------------------" << endl;
+            cout << "Empate! Comienza de nuevo..." << endl;
+            cout << "--------------------------------------------" << endl;
+            system("pause");
+            system("cls");
+            encabezadoDosJugadores();
+            cout << "|          Definicion de Turnos          |" << endl;
+            generarTiradaTurno(v, tamanio);
+            mostrarTiradaTurno(nombres, v, tamanio);
+            cout << "\n\n";
+            if(v[0] != v[1]) {
+                empate = false;
+            }
+        }
+    }
+    if(v[0] > v[1]) {
+        mostrarPrimerJugador(nombres[0]);
+    }else {
+        mostrarPrimerJugador(nombres[1]);
+        string auxiliar = nombres[0];
+        nombres[0] = nombres[1];
+        nombres[1] = auxiliar;
+    }
+}
+
 /// MODO UN JUGADOR
 void gameplayUnJugador(string nombres[], int puntajes[], int t) {
     //Ingreso de nombre
@@ -271,4 +337,102 @@ void gameplayUnJugador(string nombres[], int puntajes[], int t) {
     mostrarFinal(nombreJugador,puntajeTotal);
     actualizarPuntajes(nombres, puntajes, t, nombreJugador, puntajeTotal);
 
+}
+
+/// MODO DOS JUGADORES
+void gameplayDosJugadores(string nombres[], int puntajes[], int t) {
+    system("cls");
+
+    // Ingreso de nombres
+    encabezadoDosJugadores();
+    cout << "JUGADOR 1" << endl;
+    string nombreJugador1 = ingresarNombre();
+    cout << "\n\nJUGADOR 2" << endl;
+    string nombreJugador2 = ingresarNombre();
+
+    // Definir turnos
+    const int TAM_JUGADORES = 2;
+    int dados[TAM_JUGADORES];
+    string jugadores[TAM_JUGADORES] = {nombreJugador1, nombreJugador2};
+    system("cls");
+    definirTurno(jugadores, dados, TAM_JUGADORES);
+    system("pause");
+
+    /// GAMEPLAY
+    system("cls");
+    int puntajesJugadores[TAM_JUGADORES] = {};
+    int puntajesTotales[TAM_JUGADORES] = {};
+    int totalTurnos = 3, totalTiradas = 3;
+    const int TAM = 5;
+    int tirada [TAM];
+    string combinacion;
+
+    // Vueltas de TURNOS
+    for(int nroTurno = 1; nroTurno <= totalTurnos; nroTurno++) {
+        bool turnoTerminadoJugador1 = false;
+        bool turnoTerminadoJugador2 = false;
+        int nroTirada = 1;
+
+        //Vueltas de TIRADAS DE JUGADOR 1
+        while(turnoTerminadoJugador1 == false) {
+            system("cls");
+            encabezadoDosJugadores();
+            mostrarTurno(nroTurno, jugadores[0],puntajesTotales[0],nroTirada);
+
+            generarTirada(tirada,TAM);
+            mostrarTirada(tirada,TAM);
+
+            if(nroTirada < totalTiradas) {
+                int opcion = preguntarFinalizarTurno();
+                if(opcion == 2) {
+                    combinacion = detectarCombinacion(tirada,puntajesJugadores[0], TAM);
+                    puntajesTotales[0] += puntajesJugadores[0];
+                    mostrarResultado(puntajesJugadores[0], combinacion, puntajesTotales[0]);
+                    turnoTerminadoJugador1 = true;
+                    system("pause");
+                }
+                nroTirada++;
+            }else {
+                combinacion = detectarCombinacion(tirada,puntajesJugadores[0], TAM);
+                puntajesTotales[0] += puntajesJugadores[0];
+                mostrarResultado(puntajesJugadores[0], combinacion, puntajesTotales[0]);
+                turnoTerminadoJugador1 = true;
+                system("pause");
+            }
+        }
+
+        nroTirada = 1;
+        //Vueltas de TIRADAS DE JUGADOR 2
+        while(turnoTerminadoJugador2 == false) {
+            system("cls");
+            encabezadoDosJugadores();
+            mostrarTurno(nroTurno, jugadores[1],puntajesTotales[1],nroTirada);
+
+            generarTirada(tirada,TAM);
+            mostrarTirada(tirada,TAM);
+
+            if(nroTirada < totalTiradas) {
+                int opcion = preguntarFinalizarTurno();
+                if(opcion == 2) {
+                    combinacion = detectarCombinacion(tirada,puntajesJugadores[1], TAM);
+                    puntajesTotales[1] += puntajesJugadores[1];
+                    mostrarResultado(puntajesJugadores[1], combinacion, puntajesTotales[1]);
+                    turnoTerminadoJugador2 = true;
+                    system("pause");
+                }
+                nroTirada++;
+            }else {
+                combinacion = detectarCombinacion(tirada,puntajesJugadores[1], TAM);
+                puntajesTotales[1] += puntajesJugadores[1];
+                mostrarResultado(puntajesJugadores[1], combinacion, puntajesTotales[1]);
+                turnoTerminadoJugador2 = true;
+                system("pause");
+            }
+        }
+
+    }
+    mostrarFinal2Jugadores(jugadores,puntajesTotales,TAM_JUGADORES);
+    for(int i = 0; i < TAM_JUGADORES; i++) {
+        actualizarPuntajes(nombres, puntajes, t, jugadores[i], puntajesTotales[i]);
+    }
 }
